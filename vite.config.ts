39b9ffae8e90f40f5a6e0f418a066dcf1f176f15
Abhiram Sanjay Dharme <138222331+abhiramDharme@users.noreply.github.com>
2025-11-29 14:37:17 +0530

    Fix base URL configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/seemab.github.io/', // Add this line with your repo name
+  base: '/seemab.github.io/'
 })
